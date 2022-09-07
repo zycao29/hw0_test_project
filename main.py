e947b4751587f0a6956a63c011656ed6b5efe8f3
Zhaoyang Cao <zhaoyangcao@pepsi.dyndns.rice.edu>
2022-09-06 19:25:30 -0500

    1st commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_des():
+    # print description of task
+    print('This is hw0 task6 project edit by Zhaoyang Cao')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_des()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
